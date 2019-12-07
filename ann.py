d3c6cfe7ca5008e042f7d9af54fd8ab114d58349
Julian Allchin <julian@allchin.com>
2019-12-07 14:35:47 -0800

    Update working

--- ann.py
+++ ann.py
@@ -8,99 +8,72 @@ Created on Thu Nov 14 19:38:54 2019
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""Training set data and testing set data"""
-training_set="./mnist_train_100.csv" #short training set
-test_set="./mnist_test_10.csv"       # short test set
 
 def sigmoid(x):
 	return (1/(1+np.exp(-x)))
-	
+
 def derviativeSigmoid(x):
-	return (x) * (1 + (x))
+	return (x * (1 - x))
 
 """Artificial Neural Network Class"""
 class NeuralNetwork:
 	def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
-		
+
 		# Building layers list
 		# [inputNodes, nodesInHiddenLayer1, nodesInHiddenLayer2,..., outputNodes]
-		self.layers = [inputNodes]
+		self.layerNodeCount = [inputNodes]
 
 		for x in hiddenNodes:
-			self.layers.append(x)
-		self.layers.append(outputNodes)
+			self.layerNodeCount.append(x)
+		self.layerNodeCount.append(outputNodes)
 
+		self.lastvariance = 100
 		self.error = []
 		self.weights = []
-		self.layerOutput = []
-		self.cost = [0] * (len(self.layers) - 1)
 		self.lr = learningRate
-		
-		for i in range(1, len(hiddenNodes) + 2):
-			self.weights.append(np.random.normal(0.0, pow(self.layers[i-1], -0.5), (self.layers[i], self.layers[i-1])))
-			print("Creating weight matrix of size " + str(self.weights[len(self.weights) -1].shape))
 
-		pass
+		print("Input layer size: "+ str(self.layerNodeCount[0]))
+		# Create weight matrices between each neural layer level
+		for i in range(0, len(hiddenNodes) + 1):
+			self.weights.append(np.random.normal(0.0, pow(self.layerNodeCount[i], -0.5), (self.layerNodeCount[i+1], self.layerNodeCount[i])))
+			print("W" + str(i),"weight matrix of size " + str(self.weights[len(self.weights) -1].shape))
+		print("Output layer size: "+ str(self.layerNodeCount[i+1]))
 
 	def accuracy(self, y):
 		prob = np.copy(self.y_hat)
 		prob[prob > 0.5] = 1
 		prob[prob <= 0.5] = 0
-
 		return (prob == y).all(axis=0).mean()
 
 	def variance(self, y):
 		return np.sum(np.square(y - self.y_hat))
-	 
+
 	def forwardProp(self, X):
+		self.layerOutput = [X]
+		for i in range(0, len(self.layerNodeCount)-1):
+			# Propagate through rest
+			self.layerOutput.append(sigmoid(np.dot(self.layerOutput[i], self.weights[i].T)))
+		self.y_hat = self.layerOutput[-1] # Set y_hat to the last (output) nodes
 
-		# For each layer
-		for i in range(0, len(self.layers)-1):
-			# If its the first one
-			if i == 0:
-				# Forward prop input by weights
-				self.layerOutput.append(sigmoid(np.dot(X, self.weights[i].T)))
 
-			# Not the first one	
-			else:
-
-				# Propagate through rest
-				self.layerOutput.append(sigmoid(np.dot(self.layerOutput[i-1], self.weights[i].T)))	
-			
-		# Set y_hat to the last (output) nodes
-		self.y_hat = self.layerOutput[len(self.layerOutput) - 1 ]
-
-		pass
-		
 	def backProp(self, X, y):
-
-		# Use chain rule to find derivative of the loss with respect to weights
-		
-		self.error.append(self.variance(y))
-		
-		# For each layer   
-		for i in range(len(self.layers)-2, 0, -1):
-			# Generate a cost from the baseCost
-			if i == len(self.layers)-2:
-				errorsum = 2 * (y - self.y_hat) * derviativeSigmoid(self.y_hat)
-				prevError=errorsum.T
+		# Use Calulus chain rule to find derivative of the loss with respect to weights
+		self.lastvariance = self.variance(y)
+		# For each layer calculate the error
+		for i in range(len(self.layerNodeCount)-2, -1, -1):
+			if i == len(self.layerNodeCount)-2:
+				errorsum = (2 * (y - self.y_hat) * derviativeSigmoid(self.y_hat)).T
 			else:
-				errorsum = (np.dot(self.weights[i+1].T,prevError) * derviativeSigmoid(self.layerOutput[i].T)).T
-				prevError=errorsum
+				errorsum = (np.dot(self.weights[i+1].T, errorsum) * derviativeSigmoid(self.layerOutput[i+1]).T)
 
-			# Dotting the next layer
-			if i == 0:
-				self.cost[i] = np.dot(X.T, errorsum).T
-			else:
-				self.cost[i] = np.dot(self.layerOutput[i-1].T, errorsum).T
+			self.delta = np.dot(errorsum, self.layerOutput[i])
 
 			# Mutliplying by the learning rate
-			self.cost[i] = self.cost[i] * self.lr
+			self.delta = self.delta * self.lr
 
 			# Nudging it
-			self.weights[i] += self.cost[i]
+			self.weights[i] += self.delta
 
-				
 	# train the neural network
 	def train(self, X, y):
 		self.forwardProp(X)
@@ -118,20 +91,11 @@ class NeuralNetwork:
 		with open(file, 'rb') as f:
 			self.weights = np.load(f)
 		f.close()
-			   
+
 	def guess(self, X):
+		lastOutput = sigmoid(np.dot(X, self.weights[0].T))
 		# For each layer
-		for i in range(0, len(self.layers)-1):
-			# If its the first one
-			if i == 0:
-				# Forward prop input by weights
-				lastOutput = sigmoid(np.dot(X, self.weights[i].T))
-
-			# Not the first one	
-			else:
-
+		for i in range(1, len(self.layerNodeCount)-1):
 				# Propagate through rest
 				lastOutput = sigmoid(np.dot(lastOutput, self.weights[i].T))
-			
-		# Set y_hat to the last (output) nodes
 		return lastOutput
--- train.py
+++ train.py
@@ -1,9 +1,10 @@
 import ann
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-training_set="./mnist_train_100.csv" #short training set
+training_set="./mnist_train.csv" #short training set
 
 
 """Create instance of neural network """   
@@ -12,18 +13,26 @@ hiddenNodesCount = [200]
 outputNodeCount = 10
 learningRate = 0.001
 nn = ann.NeuralNetwork(inputNodeCount,hiddenNodesCount,outputNodeCount, learningRate)
-# nn.load('weights.npy')
+nn.load('weights.npy')
 
 """Train the ANN"""
 # load the mnist training data CSV file into a list
 training_data_file = open(training_set, 'r')
 training_data_list = training_data_file.readlines()
 training_data_file.close()
-epochs = 5
+epochs = 1000
 
-
+print()
+print("...TRAINING...")
 for e in range(epochs):
-	for record in training_data_list:  
+	debug = 'Epochs: %i \tAccuracy: %f' % (e, (1-nn.lastvariance)*100) 
+	print(debug, end="", flush=True)
+	print('\r', end='', flush=True)
+	
+	for i in range(len(training_data_list)-1):
+
+		record = training_data_list[i]
+
 		try:    
 			all_values = record.split(',') # split the record by the ',' commas
 						
@@ -34,16 +43,16 @@ for e in range(epochs):
 			X=np.array(inputs, ndmin=2) # create a 784 x 1 vector from input
 			nn.train(X, targets)
 
-			plt.clf()
-
-			plt.plot(nn.error)
-			plt.draw()
-			plt.pause(.001)
 
 		except KeyboardInterrupt:
 			print('Interrupted')
 			nn.save()
 			exit(1)
+
+
+
+
+
 
 
 nn.save()
