8e2056aadc9610a4afe16d983604c9117f6a7ef6
Jim Allchin <jim@programisprime.com>
2019-12-09 16:29:39 -0800

    clean it up some
    1. change the init to use one array to define all the node layers.
    2. changed to use the real derviative for sigmoid.  Now other activation functions can be used.
    3. allowed training to use the same large data set, but just use it up to a limit.
    4. fixed other minor things

--- ann.py
+++ ann.py
@@ -6,93 +6,90 @@ Created on Thu Nov 14 19:38:54 2019
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 def sigmoid(x):
-	return (1/(1+np.exp(-x)))
+    return (1/(1+np.exp(-x)))
 
-def derviativeSigmoid(x):
-	return (x * (1 - x))
+def derviativeSigmoid(z):
+     sig=sigmoid(z)
+     return (sig * (1 - sig))
+
 
 """Artificial Neural Network Class"""
 class NeuralNetwork:
-	def __init__(self, inputNodes, hiddenNodes, outputNodes, learningRate):
+    def __init__(self, NetSize, learningRate):
+        
+        self.layerNodeCount = NetSize
+        self.numberOfLayers = len(NetSize)
+        self.error = []
+        self.lr = learningRate
+        self.lastvariance = 100
 
-		# Building layers list
-		# [inputNodes, nodesInHiddenLayer1, nodesInHiddenLayer2,..., outputNodes]
-		self.layerNodeCount = [inputNodes]
+        # Create the neural network biases and weights arrays
+#        self.biases = [np.ones(i) for i in NetSize[1:]]
+        self.biases = [np.zeros(i) for i in NetSize[1:]]
+        self.weights=[np.random.randn(j, i)*0.1 for i, j in zip(NetSize[:-1], NetSize [1:])] 
+        
+        # Print out the sizes of the layers
+        print("Input layer size: "+ str(NetSize[0]))
+        for i in range(len(self.weights)):
+            print("W" + str(i),"weight matrix of size " + str(self.weights[i].shape))
+        print("Output layer size: "+ str(NetSize[-1]))
 
-		for x in hiddenNodes:
-			self.layerNodeCount.append(x)
-		self.layerNodeCount.append(outputNodes)
+    def accuracy(self):
+        return (1-self.lastvariance)
 
-		self.lastvariance = 100
-		self.error = []
-		self.weights = []
-		self.lr = learningRate
+    def variance(self, y):
+        return np.sum(np.square(y - self.y_hat))
 
-		print("Input layer size: "+ str(self.layerNodeCount[0]))
-		# Create weight matrices between each neural layer level
-		for i in range(0, len(hiddenNodes) + 1):
-			self.weights.append(np.random.normal(0.0, pow(self.layerNodeCount[i], -0.5), (self.layerNodeCount[i+1], self.layerNodeCount[i])))
-			print("W" + str(i),"weight matrix of size " + str(self.weights[len(self.weights) -1].shape))
-		print("Output layer size: "+ str(self.layerNodeCount[i+1]))
-
-	def accuracy(self):
-		return (1-self.lastvariance)
-
-	def variance(self, y):
-		return np.sum(np.square(y - self.y_hat))
-
-	def forwardProp(self, X):
-		self.layerOutput = [X]
-		for i in range(0, len(self.layerNodeCount)-1):
-			# Propagate through rest
-			self.layerOutput.append(sigmoid(np.dot(self.layerOutput[i], self.weights[i].T)))
-		self.y_hat = self.layerOutput[-1] # Set y_hat to the last (output) nodes
+    def forwardProp(self, X):
+        self.layerOutput = [X]
+        self.layerInput= [X]
+        for i in range(self.numberOfLayers-1):
+            self.layerInput.append(np.dot(self.layerOutput[i], self.weights[i].T)+ self.biases[i])
+            self.layerOutput.append(sigmoid(self.layerInput[-1]))  
+        self.y_hat = self.layerOutput[-1] # Set y_hat to the last (output) nodes
 
 
-	def backProp(self, X, y):
-		# Use Calulus chain rule to find derivative of the loss with respect to weights
-		self.lastvariance = self.variance(y)
-		# For each layer calculate the error
-		for i in range(len(self.layerNodeCount)-2, -1, -1):
-			if i == len(self.layerNodeCount)-2:
-				errorsum = (2 * (y - self.y_hat) * derviativeSigmoid(self.y_hat)).T
-			else:
-				errorsum = (np.dot(self.weights[i+1].T, errorsum) * derviativeSigmoid(self.layerOutput[i+1]).T)
+    def backProp(self, X, y):
+        # Use Calulus chain rule to find derivative of the loss with respect to weights
+        self.lastvariance = self.variance(y)
+        # For each layer calculate the error
+        for i in range(self.numberOfLayers-2, -1, -1):
+            if i == self.numberOfLayers-2:
+                errorsum = (2 * (y - self.y_hat) * derviativeSigmoid(self.layerInput[-1])).T
+            else:
+                errorsum = (np.dot(self.weights[i+1].T, errorsum) * derviativeSigmoid(self.layerInput[i+1]).T)
 
-			self.delta = np.dot(errorsum, self.layerOutput[i])
+            self.delta = np.dot(errorsum, self.layerOutput[i])
 
-			# Mutliplying by the learning rate
-			self.delta = self.delta * self.lr
+            # Mutliplying by the learning rate
+            self.delta = self.delta * self.lr
 
-			# Nudging it
-			self.weights[i] += self.delta
+            # Nudging it
+            self.weights[i] += self.delta
 
-	# train the neural network
-	def train(self, X, y):
-		self.forwardProp(X)
-		self.backProp(X, y)
+    # train the neural network
+    def train(self, X, y):
+        self.forwardProp(X)
+        self.backProp(X, y)
 
-	# after training save weights
-	def save(self, fname):
-		with open(fname,'wb') as f:
-			np.save(f, self.weights)
-		f.close()
-		print("Saved weights")
+    # after training save weights
+    def save(self, fname):
+        with open(fname,'wb') as f:
+            np.save(f, self.weights)
+        f.close()
+        print("Saved weights")
 
-	# after training save weights
-	def load(self, file):
-		with open(file, 'rb') as f:
-			self.weights = np.load(f)
-		f.close()
+    # after training save weights
+    def load(self, file):
+        with open(file, 'rb') as f:
+            self.weights = np.load(f)
+        f.close()
 
-	def guess(self, X):
-		lastOutput = sigmoid(np.dot(X, self.weights[0].T))
-		# For each layer
-		for i in range(1, len(self.layerNodeCount)-1):
-				# Propagate through rest
-				lastOutput = sigmoid(np.dot(lastOutput, self.weights[i].T))
-		return lastOutput
+    def guess(self, X):
+        a = X
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a) +b)
+#        print (a)
+        return a
