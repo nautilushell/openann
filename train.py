9c6aae340f71d1ff5569ce94e9846ed6f934fb9a
Julian Allchin <julian@allchin.com>
2019-12-09 16:29:39 -0800

    clean it up some
    1. change the init to use one array to define all the node layers.
    2. changed to use the real derviative for sigmoid.  Now other activation functions can be used.
    3. allowed training to use the same large data set, but just use it up to a limit.
    4. fixed other minor things

--- train.py
+++ train.py
@@ -1,19 +1,14 @@
 import ann
-import sys
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 training_set="./mnist_train.csv" #short training set
 
 
 """Create instance of neural network """   
-inputNodeCount = 784
-hiddenNodesCount = [200]
-outputNodeCount = 10
+
 learningRate = 0.001
-nn = ann.NeuralNetwork(inputNodeCount,hiddenNodesCount,outputNodeCount, learningRate)
-nn.load('weights.npy')
+nn = ann.NeuralNetwork([784,200,10], learningRate)
+#nn.load('weights.npy')
 
 """Train the ANN"""
 # load the mnist training data CSV file into a list
@@ -21,38 +16,37 @@ training_data_file = open(training_set, 'r')
 training_data_list = training_data_file.readlines()
 training_data_file.close()
 epochs = 1000
+limit=1000
 
 print()
 print("...TRAINING...")
 for e in range(epochs):
-	debug = 'Epochs: %i \tAccuracy: %f' % (e, (1-nn.lastvariance)*100) 
-	print(debug, end="", flush=True)
-	print('\r', end='', flush=True)
-	
-	for i in range(len(training_data_list)-1):
+    i=0
+    debug = '\rEpochs: %i \tAccuracy: %f' % (e, (1-nn.lastvariance)*100) 
+    print(debug, end="", flush=True)
+    
+    for i in range(len(training_data_list)-1):
+        
+        if i == limit:
+            break;
+        i+=1
 
-		record = training_data_list[i]
+        record = training_data_list[i]
 
-		try:    
-			all_values = record.split(',') # split the record by the ',' commas
-						
-			inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01 #scale
-			# create the target output values (all 0.01, except ans = 0.99)
-			targets = np.zeros(outputNodeCount) + 0.01
-			targets[int(all_values[0])] = 0.99 # all_values[0] answer for input
-			X=np.array(inputs, ndmin=2) # create a 784 x 1 vector from input
-			nn.train(X, targets)
-
-
-		except KeyboardInterrupt:
-			print('Interrupted')
-			nn.save()
-			exit(1)
+        try:    
+            all_values = record.split(',') # split the record by the ',' commas
+                        
+            inputs = (np.asfarray(all_values[1:]) / 255.0 * 0.99) + 0.01 #scale
+            # create the target output values (all 0.01, except ans = 0.99)
+            targets = np.zeros(10) + 0.01
+            targets[int(all_values[0])] = 0.99 # all_values[0] answer for input
+            X=np.array(inputs, ndmin=2) # create a 784 x 1 vector from input
+            nn.train(X, targets)
 
 
-
-
-
-
+        except KeyboardInterrupt:
+            print('Interrupted')
+            nn.save()
+            exit(1)
 
 nn.save()
